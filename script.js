802b761b192ee0dbabc51ec4d7b09ad260d9cb6b
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-08-11 18:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+function daysOfAYears(year){
+	function getDaysOfaMonth(month,year){
+		switch(month){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+				return 31
+			case 2: {
+				if(year%4 === 0){
+					if(year%100 === 0){
+						if(year%400 !== 0) return 28
+					}
+					return 29
+				}
+			}
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				return 30
+		}
+	}
+
+	let numOfDayInAYear = 0;
+	for(let i=1; i<=12; i++){
+		numOfDayInAYear += getDaysOfaMonth(i,year)
+	}
+	return numOfDayInAYear
+}
